68ec96db1615a767de741ebf01cdec4f7279e19f
Jack Grahl <jack.grahl@gmail.com>
2021-05-02 00:05:28 +0100

    Copy over the rest of the test.

--- tests/suite.lisp
+++ tests/suite.lisp
@@ -12,20 +12,6 @@
                        :interactive t)))
 
 ;;; Tests start HERE!
-  ;; NEXT-POWER-OF-TWO
-  (loop :for i :from 1 :to 25
-        :for j := (expt 2 i)
-        :do (is (= i (h::next-power-of-two j))))
-
-  (is (h::coprimep 2 3))
-  (is (h::coprimep 4 9))
-  (is (not (h::coprimep 4 20)))
-
-  (is (h::pairwise-coprimep '(2 3 5 7 11)))
-  (is (h::pairwise-coprimep #(2 3 5 7 11)))
-
-  (is (not (h::pairwise-coprimep '(2 3 5 7 9 11))))
-  (is (not (h::pairwise-coprimep #(2 3 5 7 9 11)))))
 
 (deftest test-arith-intrinsics ()
   "Test the arithmetic intrinsics."
