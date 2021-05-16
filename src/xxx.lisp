09e646a35de3b8aa9f8535e31a1c3bf3d798484f
Jack Grahl <jack.grahl@gmail.com>
2021-05-17 00:39:09 +0100

    Declaim these functions.

--- src/xxx.lisp
+++ src/xxx.lisp
@@ -1,8 +1,14 @@
 (in-package :hypergeometrica)
 
+(declaim (ftype (function ((unsigned-byte 64))
+                          (mpz))
+                x^x^x/ram))
 (defun x^x^x/ram (x)
   (mpz-expt (integer-mpz x) (expt x x)))
 
+(declaim (ftype (function ((unsigned-byte 64))
+                          (mpz))
+                x^x^x/disk))
 (defun x^x^x/disk (x)
   (let ((*maximum-vector-size* 0))
     (mpz-expt (integer-mpz x) (expt x x))))
