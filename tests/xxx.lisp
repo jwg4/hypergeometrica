6ee092a68dab9ea71673ab504fe65e53aadf8ded
Jack Grahl <jack.grahl@gmail.com>
2021-05-16 17:34:30 +0100

    Use package symbol.

--- tests/xxx.lisp
+++ tests/xxx.lisp
@@ -2,9 +2,9 @@
 
 (defun test-it (x)
     (let (ram disk)
-      (setf ram (time (x^x^x/ram x)))
-      (setf disk (time (x^x^x/disk x)))
-      (mpz-= ram disk)))
+      (setf ram (time (h::x^x^x/ram x)))
+      (setf disk (time (h::x^x^x/disk x)))
+      (h::mpz-= ram disk)))
 
 (deftest test-nine-nine-nine ()
     (test-it 9)
